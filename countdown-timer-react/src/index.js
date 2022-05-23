4654ad783523bbf43cbdfd90b3ee01879f6fda7d
greyforrest <m.voellmin.inf20@stud.bbbaden.ch>
2022-05-23 08:49:51 +0200

    layout form

--- countdown-timer-react/src/index.js
+++ countdown-timer-react/src/index.js
@@ -69,15 +69,9 @@ class DateTimeForm extends React.Component {
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <label>
-          Date:
-          <input type="date" value={this.state.valueDate} />
-        </label>
-        <label>
-          Time:
-          <input type="time" value={this.state.valueTime} />
-        </label>
-        <input type="submit" value="Submit" />
+        <label class="m-3">Date: <input type="date" class="rounded form-control" value={this.state.valueDate} /></label>
+        <label class="m-3">Time: <input type="time" class="rounded form-control" value={this.state.valueTime} /></label>
+        <input type="submit" class="btn btn-secondary m-3" value="Submit" />
       </form>
     );
   }
